7dac2ed0d5d86a3e7d01d4cf9f015a1a217a4b1e
VoDACode <dimavovk201112@gmail.com>
2023-11-06 16:08:10 +0200

    Deleted CityRegion

--- 3DForge/Models/User.cs
+++ 3DForge/Models/User.cs
@@ -20,7 +20,6 @@ namespace Backend3DForge.Models
         public string? Lastname { get; set; }
         public string? Country { get; set; }
         public string? Region { get; set; }
-        public string? CityRegion { get; set; }
         public string? City { get; set; }
         public string? Street { get; set; }
         public string? House { get; set; }
