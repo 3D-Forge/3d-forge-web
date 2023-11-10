694011e8cf15403971e63968d2d40f735eacda77
VoDACode <dimavovk201112@gmail.com>
2023-11-10 21:16:38 +0200

    ⬆️Model calculator improved

--- 3DForge/Services/ModelCalculator/ModelCalculator.cs
+++ 3DForge/Services/ModelCalculator/ModelCalculator.cs
@@ -1,6 +1,5 @@
 ﻿using Backend3DForge.Services.ModelCalculator.Models;
 using QuantumConcepts.Formats.StereoLithography;
-using System.Text;
 
 namespace Backend3DForge.Services.ModelCalculator
 {
@@ -21,12 +20,6 @@ namespace Backend3DForge.Services.ModelCalculator
                 case "stl":
                     result = CalculateStlSurfaceArea(modelStream);
                     break;
-                case "stp":
-                    result = await CalculateStpSurfaceArea(modelStream);
-                    break;
-                case "step":
-                    result = await CalculateStpSurfaceArea(modelStream);
-                    break;
                 case "obj":
                     result = await CalculateObjSurfaceArea(modelStream);
                     break;
@@ -37,49 +30,28 @@ namespace Backend3DForge.Services.ModelCalculator
             return result;
         }
 
-        private async Task<ModelCalculatorResult> CalculateStpSurfaceArea(Stream stream)
-        {
-            throw new NotImplementedException();
-        }
-
         private async Task<ModelCalculatorResult> CalculateObjSurfaceArea(Stream modelStream)
         {
-
-            byte[] modelData;
-
-            using (var ms = new MemoryStream())
-            {
-                await modelStream.CopyToAsync(ms);
-                modelData = ms.ToArray();
-            }
-
-            ObjModel objModel;
-
-            using (var ms = new MemoryStream(modelData))
-            {
-                objModel = await ObjReader.ReadObjFile(ms);
-            }
+            ObjModel objModel = await ObjReader.ReadObjFile(modelStream);
 
             var xSize = objModel.Vertices.Max(p => p.X) - objModel.Vertices.Min(p => p.X);
             var ySize = objModel.Vertices.Max(p => p.Y) - objModel.Vertices.Min(p => p.Y);
             var zSize = objModel.Vertices.Max(p => p.Z) - objModel.Vertices.Min(p => p.Z);
             var surfaceArea = objModel.Faces.Sum(p => CalculateArea(p.Vertices));
-            double volume = objModel.Faces.Sum(p => SignedVolumeOfTriangle(p.Vertices));
+            double volume = objModel.Faces.Sum(p => SignedVolumeOfTriangle(p.Vertices)) * 2;
 
             return new ModelCalculatorResult
             {
-                SurfaceArea = surfaceArea,
-                Volume = volume,
-                X = xSize,
-                Y = ySize,
-                Z = zSize
+                SurfaceArea = Math.Round(surfaceArea, 0),
+                Volume = Math.Round(volume, 0),
+                X = Math.Round(xSize, 2),
+                Y = Math.Round(ySize, 2),
+                Z = Math.Round(zSize, 2)
             };
         }
         public ModelCalculatorResult CalculateStlSurfaceArea(Stream modelStream)
         {
             STLDocument stl = STLDocument.Read(modelStream);
-
-            var facets = stl.ToArray();
 
             // size in mm
             double xSize = stl.Max(p => p.Vertices.Max(v => v.X)) - stl.Min(p => p.Vertices.Min(v => v.X));
@@ -87,15 +59,16 @@ namespace Backend3DForge.Services.ModelCalculator
             double zSize = stl.Max(p => p.Vertices.Max(v => v.Z)) - stl.Min(p => p.Vertices.Min(v => v.Z));
             // mm^2
             double surfaceArea = stl.Sum(p => CalculateArea(p.Vertices.ToVector3()));
+            // mm^3
             double volume = stl.Sum(p => SignedVolumeOfTriangle(p.Vertices.ToVector3()));
 
             return new ModelCalculatorResult
             {
-                SurfaceArea = surfaceArea,
-                Volume = volume,
-                X = xSize,
-                Y = ySize,
-                Z = zSize
+                SurfaceArea = Math.Round(surfaceArea, 0),
+                Volume = Math.Round(volume, 0),
+                X = Math.Round(xSize, 2),
+                Y = Math.Round(ySize, 2),
+                Z = Math.Round(zSize, 2)
             };
         }
 
@@ -115,19 +88,7 @@ namespace Backend3DForge.Services.ModelCalculator
 
             return totalArea;
         }
-
-        private double CalculateTriangleArea(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3)
-        {
-            double side1 = Math.Sqrt(Math.Pow(vertex2.X - vertex1.X, 2) + Math.Pow(vertex2.Y - vertex1.Y, 2) + Math.Pow(vertex2.Z - vertex1.Z, 2));
-            double side2 = Math.Sqrt(Math.Pow(vertex3.X - vertex2.X, 2) + Math.Pow(vertex3.Y - vertex2.Y, 2) + Math.Pow(vertex3.Z - vertex2.Z, 2));
-            double side3 = Math.Sqrt(Math.Pow(vertex1.X - vertex3.X, 2) + Math.Pow(vertex1.Y - vertex3.Y, 2) + Math.Pow(vertex1.Z - vertex3.Z, 2));
-
-            double semiPerimeter = (side1 + side2 + side3) / 2;
-            double triangleArea = Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
-
-            return triangleArea;
-        }
-
+        
         private double SignedVolumeOfTriangle(IEnumerable<Vector3> vertices)
         {
             Vertex p1 = vertices.ElementAt(0);
@@ -142,6 +103,18 @@ namespace Backend3DForge.Services.ModelCalculator
             double v123 = p1.X * p2.Y * p3.Z;
 
             return (1.0 / 6.0) * (-v321 + v231 + v312 - v132 - v213 + v123);
+        }
+
+        private double CalculateTriangleArea(Vector3 vertex1, Vector3 vertex2, Vector3 vertex3)
+        {
+            double side1 = Math.Sqrt(Math.Pow(vertex2.X - vertex1.X, 2) + Math.Pow(vertex2.Y - vertex1.Y, 2) + Math.Pow(vertex2.Z - vertex1.Z, 2));
+            double side2 = Math.Sqrt(Math.Pow(vertex3.X - vertex2.X, 2) + Math.Pow(vertex3.Y - vertex2.Y, 2) + Math.Pow(vertex3.Z - vertex2.Z, 2));
+            double side3 = Math.Sqrt(Math.Pow(vertex1.X - vertex3.X, 2) + Math.Pow(vertex1.Y - vertex3.Y, 2) + Math.Pow(vertex1.Z - vertex3.Z, 2));
+
+            double semiPerimeter = (side1 + side2 + side3) / 2;
+            double triangleArea = Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+
+            return triangleArea;
         }
 
     }
